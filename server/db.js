3a2592a7143953f7fa45ca5dccea4469dac2bd12
alexml <alexgbrewster@gmail.com>
2024-12-10 18:28:25 -0500

    hi

--- server/db.js
+++ server/db.js
@@ -1,11 +1,5 @@
 const mysql = require('mysql2');
 require('dotenv').config();
-const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'alexml',
-    password: 'Trtploergvui1',
-    database: 'mysqltodo',
-    port: 3306
-});
+const connection = mysql.createConnection(process.env.MYSQL_URL);
 
 module.exports = connection;
