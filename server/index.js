70d1af0ceda74d07ba00ee2ef10c64192998a97e
alexml <alexgbrewster@gmail.com>
2024-12-10 18:31:48 -0500

    hi

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT
+const port = process.env.PORT || 8080;
 
 // Parse JSON bodies for POST requests
 app.use(express.json());
